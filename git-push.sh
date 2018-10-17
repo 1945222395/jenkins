d55fbc878db2c781a8d54c57e007d901b4e889ed
root <root@prd-zldp-web01.nj>
2018-10-17 18:34:32 +0800

    2018-10-17_18:34:32 commit

---
+++ git-push.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+git-branch () {
+git init .
+git remote rm origin
+git remote add origin http://github.com:443/lyc2395/jenkins.git
+git config credential.helper store
+git checkout  old-version-deploy-scripts
+git checkout -b  old-version-deploy-scripts
+}
+git-branch
+git checkout  old-version-deploy-scripts
+git checkout -b  old-version-deploy-scripts
+git commit -m "$(date +%F_%H:%M:%S) commit"
+git status
+git add .
+git push -f -u origin old-version-deploy-scripts
