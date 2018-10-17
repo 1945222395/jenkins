2b5cc15e1c91f0b7b58061d4a87b4758fccc0a26
root <root@prd-jenkins01.nj>
2018-10-17 18:45:33 +0800

    2018-10-17_18:45:33 commit

--- git-push.sh
+++ git-push.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
+export http_proxy=10.129.54.27:3128
 git-branch () {
-git init .
 git remote rm origin
 git remote add origin https://github.com/lyc2395/jenkins.git
 git config credential.helper store
-git checkout  old-version-deploy-scripts
-git checkout -b  old-version-deploy-scripts
+git checkout old-version-deploy-scripts
+git checkout -b old-version-deploy-scripts
 }
 #git-branch
 git checkout  old-version-deploy-scripts
-git checkout -b  old-version-deploy-scripts
-git commit -m "$(date +%F_%H:%M:%S) commit"
 git status
+git commit -m "$(date +%F_%H:%M:%S) commit"
 git add .
 git push -f -u origin old-version-deploy-scripts
