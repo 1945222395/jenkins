ce6c34e6c62dfe8b60db0caf38d236553263c909
Cloud User <appuser@prd-jenkins01.nj>
2018-10-17 11:15:57 +0800

    2018-10-17_11:15:57 commit

---
+++ git-push.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+export http_proxy=10.129.54.27:3128
+git-branch () {
+git remote rm origin
+git remote add origin https://github.com/lyc2395/mysql.git
+git config credential.helper store
+git checkout prd-deploy
+git checkout -b prd-deploy
+}
+
+git checkout  prd-deploy
+git status
+git add .
+git push -f -u origin prd-deploy
